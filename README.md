f4eb14664a62de34e26ef46e88c2319aca1e85c4
upquark00 <miranda.bohm@yahoo.com>
2020-06-15 23:30:47 -0700

    Add data import and visualization, update README

--- README.md
+++ README.md
@@ -8,5 +8,17 @@
 - If possible, find and model Depression-era-specific data
 - Compare predictions made via sentiment analysis
 
+## Data 
+The model was trained on the 'DJIA 30-Stock Time Series Dataset' from Kaggle, which can be found here: 
+
+https://www.kaggle.com/szrlee/stock-time-series-20050101-to-20171231
+
+This set includes 13 years of stock data ranging from January 3, 2006, through December 29, 2017, thus encompassing the 2008 Financial Crisis, and therefore potentially able to provide insights into the 2020 stock market crash
+
+The goal is to gainful useful insights pertaining to Q2 and Q3 trends which have yet to unfold amidst the global COVID-19 health crisis. 
+
 ## Dependencies
 { TBD }
+
+## Project Structure
+Datasets will be contained in a subdirectory named 'data.'
--- RNN.py
+++ RNN.py
@@ -2,11 +2,35 @@
 # Created on Mon Jun 15 17:18:52 2020
 # @author: Upquark00
 
+import os
 import tensorflow as tf
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from time import strptime
 
 # Load data 
+file = r'all_stocks_2006-01-01_to_2018-01-01' + '.csv'
+dirname = os.path.dirname(__file__)
+filename = os.path.join((dirname), 'data/' +file)
 
-# Visualize data
+df = pd.read_csv(filename)
+tickers = df.Name.unique().tolist()
+
+# Start with one stock
+apple = tickers[2]
+
+# Visualize the total dataset
+for ticker in tickers: 
+    data = df[df.Name.eq(ticker)]
+    dates = df['Date'][:len(data)]
+    adj_dates = mdates.datestr2num(dates)
+    plt.plot_date(adj_dates, data['Close'], '-')
+    
+plt.title('Closing Value vs. Date for 30 Top DJIA Equities')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close value', fontsize=16)
+plt.show()
 
 # Process data 
 

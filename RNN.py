c2bfc23a7ce56f04b36f36beda1a68e77c79ae0e
upquark00 <miranda.bohm@yahoo.com>
2020-06-20 20:49:35 -0700

    Fix dataset structure

--- RNN.py
+++ RNN.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-# Created on Mon Jun 15 17:18:52 2020
-# @author: Upquark00
-
 import os
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Activation, Dense, Dropout
-from tensorflow.keras.metrics import Precision, Recall, TruePositives, TrueNegatives, FalsePositives, FalseNegatives
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from time import strptime
 from math import ceil
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler 
 
 # Load data 
 file = r'DJIA_historical' + '.csv'
@@ -36,86 +31,58 @@ plt.show()
 if data.isna().sum():
     print('Alert! Missing data')
     # do something to handle empty data rows 
+    
+# Define the training set
+percent_training = 0.80
+num_training_samples = ceil(percent_training*len(df))
+training_set = df.iloc[:num_training_samples, 5:6].values
 
-# Normalization is not ideal since closing values trend upwards. 
-# Standardization is not appropriate since data does not approximate a Gaussian
+# Scale training data
+scaler = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = scaler.fit_transform(training_set)
 
-# Convert data into format acceptable to Keras
-# Input must be 3D ndarray of shape (samples x time steps x features)
-train_fraction = 0.80
+# Some parameters
 sequence_length = 30
 
-data = data.to_numpy()
-scaler = MinMaxScaler(feature_range = (0, 1))
-data2 = data.reshape(-1, 1)
-data = scaler.fit_transform(data2)
+# Build x and y components 
+x_train = np.array([training_set_scaled[i - sequence_length:i, 0] for i in range(sequence_length, len(training_set_scaled))])
+y_train = np.array([training_set_scaled[i, 0] for i in range(sequence_length, len(training_set_scaled))])
 
-num_train_samples = ceil(len(data) * train_fraction)
-train_data = data[:num_train_samples]
-test_data = data[num_train_samples:]
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) # (7105, 30, 1)
 
-# Reshape data 
-# x_train.shape = (7105,30)
-# y_train.shape = (7105,)
+# Define Test Set 
+num_testing_samples = len(df) - x_train.shape[0] # 1813
+testing_set = df.iloc[-num_testing_samples:, 5:6].values
+testing_set_scaled = scaler.fit_transform(testing_set)
 
-x_train_0 = np.array([train_data[i - sequence_length:i] for i in range(sequence_length, len(train_data))])
-y_train_0 = np.array([train_data[i + 1 - sequence_length: (i + 1)] for i in range(sequence_length, len(train_data))])
-# x_train_0.shape = (7105,30)
-# y_train_0.shape = (7105, 30)
-
-x_train = np.reshape(x_train_0, (x_train_0.shape[0], x_train_0.shape[1], 1))
-y_train = np.reshape(y_train_0, (y_train_0.shape[0], y_train_0.shape[1], 1))
-
-# x_train.shape should now be (7105,30, 1)
-
-x_test = np.array([test_data[i - sequence_length:i] for i in range(sequence_length, len(test_data))])
-y_test = np.array([test_data[i + 1 - sequence_length: (i + 1)] for i in range(sequence_length, len(test_data))])
-
-# Test shape is (1753, 30, 1)
-# Trim the test samples
-x_test, y_test = x_test[:1750, :], y_test[:1750, :] 
-
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
-
+x_test = np.array([testing_set_scaled[i - sequence_length:i, 0] for i in range(sequence_length, len(testing_set_scaled))])
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1)) # (7105, 30, 1)
+y_test = np.array([testing_set_scaled[i, 0] for i in range(sequence_length, len(testing_set_scaled))])
 
 # Build model 
-units = 1
-epochs = 50
-batch_size = 35
+epochs = 100
+batch_size = 32
 
-# Build model 
-layer_1 = LSTM(
-    units = 25, 
-    batch_input_shape = (batch_size, sequence_length, 1),
+# Describe layers
+LSTM_1 = LSTM(
+    units = 50, 
+    input_shape = (x_train.shape[1], 1),
     return_sequences = True,
-    stateful = True, 
-    kernel_initializer = 'random_uniform'
     )
 # Output shape is (batch_size, steps, units)
 
-layer_2 = LSTM(
-    units = units,
-    return_sequences = True,
-    stateful = True, 
-    kernel_initializer = 'random_uniform'    
-    )
-
 model = Sequential()
-model.add(layer_1)
-model.add(layer_2)
-model.add(Dense(1, activation = 'sigmoid'))
+model.add(LSTM_1)
+model.add(Dropout(0.2))
+
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
+
+model.add(Dense(1))
 model.compile(loss = 'mean_squared_error', 
              optimizer = 'adam', 
              metrics = [
-                 'accuracy', 
-                 Precision(name='precision'), 
-                 Recall(name='recall'),
-                 TruePositives(name='TP'),
-                 TrueNegatives(name='TN'),
-                 FalsePositives(name='FP'),
-                 FalseNegatives(name='FN')])
-
+                 'accuracy'])
 
 # =============================================================================
 # early_stopping = EarlyStopping(monitor='val_loss', 
@@ -136,7 +103,8 @@ history = model.fit(x_train,
 
 # Evaluate performance 
 model.summary()
-loss, accuracy, precision, recall, TP, TN, FP, FN = model.evaluate(x_test, y_test, batch_size = batch_size)
+loss, accuracy = model.evaluate(x_test, y_test, batch_size = batch_size)
+
 print('Test Loss: %f' % (loss))
 print('Test Accuracy: %f' % (accuracy * 100))
 # print('Training stopped after',early_stopping.stopped_epoch,'epochs.')
@@ -154,54 +122,4 @@ plt.ylabel('loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-
-plt.plot(history.history['precision'])
-plt.title('Model Precision vs. Epoch')
-plt.ylabel('Precision')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['recall'])
-plt.title('Model Recall vs. Epoch')
-plt.ylabel('Recall')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-loss = model.evaluate(x_test, y_test)
-# print('Test Loss: %f' % (loss))
-# print('Test Accuracy: %f' % (accuracy * 100))
-
-history = model.history.history
-
-# =============================================================================
-# plt.plot(history['accuracy'])
-# plt.title('Model Accuracy vs. Epoch')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['Train', 'Test'], loc='upper left')
-# plt.show()
-# =============================================================================
-
-plt.plot(history['loss'])
-plt.title('Model Loss vs. Epoch')
-plt.ylabel('loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
